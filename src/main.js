e34d770a150f5936cf6d0a09362ae510b81ffb00
midbezoar <2974490550@qq.com>
2022-05-28 19:07:16 +0800

    build dist

--- src/main.js
+++ src/main.js
@@ -1,17 +1,67 @@
-$('addBtn').on('click', () => {
-  const url = window.prompt('请问你要添加的网址是啥？');
-  let hUrl;
-  if (url.indexOf('http') !== 0) {
-    hUrl = 'https://' + url;
-  }
-  const $siteList = $('.siteList');
-  const $lastLi = $siteList.find('li.last');
-  const $li = $(`<li>
-    <a href="${hUrl}">
+const $siteList = $('.siteList');
+const $lastLi = $siteList.find('li.last');
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: 'A', url: 'https://www.acfun.cn/' },
+  { logo: 'B', url: 'https://www.bilibili.com' },
+];
+
+const simplifyUrl = (url) => {
+  return url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, '');
+};
+
+const render = () => {
+  $siteList.find('li:not(.last)').remove();
+
+  hashMap.forEach((item, index) => {
+    const $li = $(`<li>
       <div class="site">
-        <div class="logo">${url[0]}</div>
-        <div class="link">${url}</div>
+        <div class="logo">${item.logo}</div>
+        <div class="link">${simplifyUrl(item.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
       </div>
-    </a>
-  </li>`).insertBefore($lastLi);
+    </li>`).insertBefore($lastLi);
+    $li.on('click', () => {
+      window.open(item.url);
+    });
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation(); // 阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+
+$('.add-btn').on('click', () => {
+  let url = window.prompt('请问你要添加的网址是啥？');
+  if (url.indexOf('http') !== 0) {
+    url = 'https://www.' + url;
+  }
+  hashMap.push({ logo: simplifyUrl(url)[0].toUpperCase(), url: url });
+  render();
+});
+
+// 把 hashMap 存储
+window.onbeforeunload = () => {
+  // JSON.stringify 可以把一个对象变成一个字符串
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem('x', string);
+};
+
+// 键盘事件打开页面
+// document.addEventListener()
+$(document).on('keypress', (e) => {
+  const {key} = e
+  for(let i = 0; i<hashMap.length;i++){
+    if(hashMap[i].logo.toLowerCase() === key){
+      window.open(hashMap[i].url)
+    }
+  }
 });
